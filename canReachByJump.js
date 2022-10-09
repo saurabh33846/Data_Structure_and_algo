7b4e956f5833b9bedb7fb528ff52e6d533355f06
Saurabh kumar <43277898+saurabh33846@users.noreply.github.com>
2022-10-09 18:30:24 +0530

    Create canReachByJump.js

---
+++ canReachByJump.js
@@ -0,0 +1,58 @@
+/**
+ * @param {string} s
+ * @param {number} minJump
+ * @param {number} maxJump
+ * @return {boolean}
+ */
+var canReach = function(s, minJump, maxJump) {
+    let left = minJump;
+    let right = maxJump;
+    let len = s.length -1;
+     if(left<=len && right>=len && s[len]==='0')
+    {
+        return true;
+    }
+    while(true) {
+        let i = left;
+        let noZero = true;
+        let curMaxRight = right
+        let curMinLeft = left;
+        let firstZero = true;
+       
+        if(  s[len]==='1') {
+            return false
+        }
+         if(left > len) {
+             return false;
+         }
+        let tempMinLeft = curMinLeft;
+        while(i<=right) {
+            if(s[i]==='0') {
+                noZero = false;
+                curMaxRight = i+maxJump;
+                tempMinLeft = i+ minJump;
+                if (tempMinLeft > len) {
+                    return false;
+                }
+                if(tempMinLeft<=len && curMaxRight>=len)
+                {
+                    return true;
+                }
+                if(tempMinLeft > right) {
+                    curMinLeft = tempMinLeft;
+                }
+                
+            }
+            i++;
+        }
+        if (noZero) {
+            return false;
+        }
+        if (curMinLeft< right) {
+            return false;
+        }
+        left = curMinLeft;
+        right = curMaxRight;
+        
+    }
+};
